f449b04c7d41bcc1a0ddf17e690d00da66d15041
Dan Galbraith <dan@eidria.com>
2024-06-07 14:06:45 -0700

    Update Package.swift
    Bumped minimum supported iOS version to 17, to add @Observable support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DirectoryChangeWatcher",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v14)
     ],
     products: [
